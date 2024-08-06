6f29d3e780cd319acc054e0462fca0cf07e45557
yogesh <yogesh001@gmail.com>
2024-08-06 07:35:06 -0400

    latest commit

--- day-5-ec2userdata/test.sh
+++ day-5-ec2userdata/test.sh
@@ -2,6 +2,6 @@
 sudo yum update -y
 sudo yum install git -y
 sudo yum install -y httpd
-sudo systemctl httpd start  
+sudo systemctl start httpd  
 sudo systemctl enable httpd
 echo "<h1>Welcome to devops by using terraform</h1>" > /var/www/html/index.html
